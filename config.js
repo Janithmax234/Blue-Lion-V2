ea9df10118e6574aee2d2c4f744b0e3dc0f7a9c5
Janithmax234 <109205729+Janithmax234@users.noreply.github.com>
2023-08-27 06:01:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const fs = require('fs-extra')
   
   
  //═══════[Required Variables]════════\\ 
- global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.owner = process.env.OWNER_NUMBER.split("94752013704") 
  global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/nipuna15' 
- global.location = 'Sri lanka, Monaragala' 
+ global.location = 'Sri lanka, hettipola' 
  global.gurl = 'https://instagram.com/' // add your username 
  global.sudo = process.env.SUDO || '919628516236' 
  global.devs = '919628516236'; 
  global.website = 'https://github.com/nipuna15/Blue-Lion-V2' //wa.me/+91000000000000 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/nipuna15/nipuna15/main/IMG-20230613-WA0008.jpg' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? 'Blue-Lion' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? 'nipuna' : process.env.OWNER_NAME,  
-   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'Nipuna' : process.env.PACK_INFO.split(";")[0], 
+   botname:   process.env.BOT_NAME === undefined ? 'Blue-Lion 4flag team' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? 'janith' : process.env.OWNER_NAME,  
+   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'janith' : process.env.PACK_INFO.split(";")[0], 
    auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Blue-lion' : process.env.PACK_INFO.split(";")[1],
    sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
-   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
+   autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
    alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
-   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
+voice  :  process.env.AUTO_VOICE === undefined ? true : process.env.AUTO_VOICE, 
+   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '94' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
    auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
    HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
@@ -35,7 +35,7 @@ voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE,
    antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES, 
    antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION, 
    BRANCH: 'main',  
-   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE, 
+   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? 'I am alive bro' : process.env.ALIVE_MESSAGE, 
    autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO, 
    OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY, 
    heroku:  process.env.heroku === undefined ? false : process.env.heroku, 
